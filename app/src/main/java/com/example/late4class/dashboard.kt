9ec500e93da9bce6d4931659c4d3a626819da220
peammapake <34181322+peammapake@users.noreply.github.com>
2019-11-23 20:54:19 +0700

    new test

--- app/src/main/java/com/example/late4class/dashboard.kt
+++ app/src/main/java/com/example/late4class/dashboard.kt
@@ -1,15 +1,38 @@
 package com.example.late4class
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import android.widget.Toast
 
 class Dashboard : AppCompatActivity() {
-
-    class FirebaseRef
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-        R.id.firebaseTextView.firebaseTextViewl
+
+        button.setOnClickListener{
+            Toast.makeText(this, "Activity not yet implement", Toast.LENGTH_LONG).show()
+        }
+        button2.setOnClickListener{
+            Toast.makeText(this, "Activity not yet implement", Toast.LENGTH_LONG).show()
+        }
+        button3.setOnClickListener{
+            Toast.makeText(this, "Activity not yet implement", Toast.LENGTH_LONG).show()
+        }
+        button4.setOnClickListener{
+            Toast.makeText(this, "Activity not yet implement", Toast.LENGTH_LONG).show()
+        }
+        button5.setOnClickListener{
+            Toast.makeText(this, "Activity not yet implement", Toast.LENGTH_LONG).show()
+        }
+        button6.setOnClickListener{
+            Toast.makeText(this, "Activity not yet implement", Toast.LENGTH_LONG).show()
+        }
+        button7.setOnClickListener{
+            Toast.makeText(this, "Activity not yet implement", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
