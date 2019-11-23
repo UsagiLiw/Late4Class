5e014d8da983542f91795d875ef7d4d11f0003f5
peammapake <34181322+peammapake@users.noreply.github.com>
2019-11-23 16:52:41 +0700

    new test

--- app/src/main/java/com/example/late4class/dashboard.kt
+++ app/src/main/java/com/example/late4class/dashboard.kt
@@ -3,11 +3,13 @@ package com.example.late4class
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class dashboard : AppCompatActivity() {
+class Dashboard : AppCompatActivity() {
+
+    class FirebaseRef
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        R.id.firebaseTextView.firebaseTextViewl
     }
 }
-
